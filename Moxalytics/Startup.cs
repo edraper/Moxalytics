17e01a4ab97c0a02a005841028e10f5d4d634703
unknown <edraper@ED-PC.moxtek.local>
2014-09-17 20:57:46 -0600

    First Commit

---
+++ Moxalytics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Moxalytics.Startup))]
+namespace Moxalytics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
