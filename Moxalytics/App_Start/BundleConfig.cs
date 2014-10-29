ff9b973f89a6b860b29c8f2a35ebdf8ead391c3c
tlenhart <tlenhart@users.noreply.github.com>
2014-10-28 22:40:41 -0600

    Added DBUI.js to bundle config.

--- Moxalytics/App_Start/BundleConfig.cs
+++ Moxalytics/App_Start/BundleConfig.cs
@@ -19,7 +19,8 @@ namespace Moxalytics
                 "~/Scripts/angular.js",
                 "~/Scripts/angular-animate.js",
                 "~/Scripts/angular-route.js",
-                "~/Scripts/moxalytics.js"));
+                "~/Scripts/moxalytics.js",
+                "~/Scripts/DBUI.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
