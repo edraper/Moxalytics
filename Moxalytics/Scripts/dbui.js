9a2548876d0c496d325a416bc7fb93f34800cac5
Reuben Perry <Dethlore@users.noreply.github.com>
2014-11-19 21:50:45 -0700

    sandbox on About for UI

--- Moxalytics/Scripts/dbui.js
+++ Moxalytics/Scripts/dbui.js
@@ -1,3 +1,14 @@
-﻿moxalytics.controller('DBUIController', ['$scope', function ($scope) {
+﻿moxalytics.controller('DBUIController', ['$scope', function ($scope, $http) {
     $scope.TEXT1 = "DOOBY";
+    $scope.databases = [];
+    /*$http.get(DB1JSON).then(function (res) {
+        $scope.dbjsonres = res.data;
+    });*/
+
 }]);
+
+var DB1JSON = "{'DBName':'First Database', " +
+    "'TableList': [" +
+    "{'TableName': 'Table1', 'FieldList': [{'FieldName':'Field1'}, {'FieldName':'field2'}]}, " + 
+    "{'TableName': 'Table2', 'FieldList': [{'FieldName':'Field1'}, {'FieldName':'field2'}]}, " +
+    "]}";
